b61c13419c7bd9f08ccebef367f3d3c68c93af72
oleg578 <oleg.nagornij@gmail.com>
2024-07-14 15:18:33 +0200

    Refactor sql2json for enhanced error handling and struct creation
    Improved the sql2json package by enhancing error handling when processing column names and creating a more efficient mapping of column data to structs. The revised code now prepares column names upfront and maps rows of data to structs instead of maps, adding robustness and efficiency to the SQL to JSON conversion process. Removed the redundant function `assignCellValue`.

--- sql2json/sql2json.go
+++ sql2json/sql2json.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"regexp"
 )
 
@@ -12,36 +13,32 @@ func RowsToJson(rows *sql.Rows) ([]byte, error) {
 		return nil, fmt.Errorf("rows is nil")
 	}
 	columns, err := rows.Columns()
+	var errSE error
+	for i := 0; i < len(columns); i++ {
+		columns[i], errSE = spaceToUnderscore(columns[i])
+		if errSE != nil {
+			return nil, fmt.Errorf("names of columns is invalid: %s", errSE.Error())
+		}
+	}
 	if err != nil {
 		return nil, err
 	}
-	result := make([]map[string]interface{}, 0)
+	result := make([]interface{}, 0)
 	values, valuePtrs := createPtrs(len(columns))
 	for rows.Next() {
 		if err := rows.Scan(valuePtrs...); err != nil {
 			return nil, err
 		}
-		rowMap := make(map[string]interface{})
-		for i, col := range columns {
-			colName, err := spaceToUnderscore(col)
-			if err != nil {
-				return nil, err
-			}
-			rowMap[colName] = assignCellValue(values[i])
+		structRow, errSt := slcToStruct(columns, values)
+		if errSt != nil {
+			panic(errSt)
 		}
-		result = append(result, rowMap)
+		result = append(result, structRow.Interface())
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 	return json.Marshal(result)
-}
-
-func assignCellValue(val interface{}) interface{} {
-	if b, ok := val.([]byte); ok {
-		return string(b)
-	}
-	return val
 }
 
 // Returns slice of pointers
@@ -52,6 +49,33 @@ func createPtrs(num int) ([]interface{}, []interface{}) {
 		ptrs[i] = &vals[i]
 	}
 	return vals, ptrs
+}
+
+// slcToStruct takes in two slices, names and values, and creates a new struct type
+// with the specified field names and corresponding types. It returns the created struct type
+// as a 'reflect.Type' and an error if the number of names and values are not equal.
+func slcToStruct(names []string, values []interface{}) (reflect.Value, error) {
+	if len(names) != len(values) {
+		return reflect.Value{}, fmt.Errorf("number of names and values are not equal")
+	}
+	var structFields []reflect.StructField
+	for i, name := range names {
+		structFields = append(structFields, reflect.StructField{
+			Name: name,
+			Type: reflect.TypeOf(values[i]),
+			Tag:  reflect.StructTag(`json:"` + name + `"`),
+		})
+	}
+	sType := reflect.StructOf(structFields)
+	// Create a new instance of the struct
+	sValue := reflect.New(sType).Elem()
+
+	// Set the values of the struct fields
+	for i, fName := range names {
+		sValue.FieldByName(fName).Set(reflect.ValueOf(values[i]))
+	}
+
+	return sValue, nil
 }
 
 func spaceToUnderscore(input string) (string, error) {
