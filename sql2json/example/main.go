2ec6ab93529a0aa6918b892c7893a384ab7ec4d5
oleg578 <oleg.nagornij@gmail.com>
2024-07-15 09:08:51 +0200

    Add main.go example for sql2json package
    This commit introduces a new example file, main.go for the sql2json package. It demonstrates how to use the package's functionality by creating mock SQL rows and converting them to JSON. Additionally, it includes a function to print memory usage for performance tracking.

---
+++ sql2json/example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
+	"runtime"
+	"sql2json"
+)
+
+func main() {
+	db, mock, _ := sqlmock.New()
+	defer db.Close()
+	rows := sqlmock.NewRows([]string{"Column 1", "Column 2"})
+	for i := 0; i < 1000000; i++ {
+		rows.AddRow(fmt.Sprintf("Dummy_%d", i), i)
+	}
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+	rs, _ := db.Query("SELECT 1")
+
+	out, err := sql2json.RowsToJson(rs)
+	if err != nil {
+		panic(err)
+	}
+	printMemUsage()
+	fmt.Println(len(out))
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB", m.Alloc/1024/1024)
+	fmt.Printf("\tTotalAlloc = %v MiB", m.TotalAlloc/1024/1024)
+	fmt.Printf("\tHeapAlloc = %v MiB", m.HeapAlloc/1024/1024)
+	fmt.Printf("\tStackInuse = %v MiB", m.StackInuse/1024/1024)
+	fmt.Printf("\tSys = %v MiB", m.Sys/1024/1024)
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
