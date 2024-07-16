8ec6a47f234b256b95016e052cbc20af239a5a52
oleg578 <oleg.nagornij@gmail.com>
2024-07-16 11:27:16 +0200

    Refactor result initialization in sql2json
    The initializing process has been streamlined for the result array in sql2json.go. This modification provides a more concise way to initialize the array by using 'var' instead of the previous 'make' method. This revision ensures cleaner and more maintainable code.

--- sql2json/sql2json.go
+++ sql2json/sql2json.go
@@ -20,7 +20,7 @@ func RowsToJson(rows *sql.Rows) ([]byte, error) {
 	if len(columns) == 0 {
 		return nil, fmt.Errorf("no columns found")
 	}
-	result := make([]interface{}, 0)
+	var result []interface{}
 	values, valuePtrs := createPtrs(len(columns))
 	structRow := buildStruct(columns)
 	for rows.Next() {
