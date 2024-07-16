00ed87bc4703116031c65a54807859ead5860a83
oleg578 <oleg.nagornij@gmail.com>
2024-07-16 15:36:17 +0200

    Initialize "result" array with "make" in sql2json.go

--- sql2json/sql2json.go
+++ sql2json/sql2json.go
@@ -16,7 +16,7 @@ func RowsToJsonMapping(rows *sql.Rows) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	var result []map[string]interface{}
+	result := make([]map[string]interface{}, 0)
 	values, valuePtrs := createPtrs(len(columns))
 	for rows.Next() {
 		if err := rows.Scan(valuePtrs...); err != nil {
@@ -64,7 +64,7 @@ func RowsToJsonReflection(rows *sql.Rows) ([]byte, error) {
 	if len(columns) == 0 {
 		return nil, fmt.Errorf("no columns found")
 	}
-	var result []interface{}
+	result := make([]interface{}, 0)
 	values, valuePtrs := createPtrs(len(columns))
 	structRow := buildStruct(columns)
 	for rows.Next() {
