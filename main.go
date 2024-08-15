89e4d3084b93aa3cbbd24a0daa45c3ab03feaad7
oleg578 <oleg.nagornij@gmail.com>
2024-08-15 11:30:05 +0200

    git ignore

--- main.go
+++ main.go
@@ -1,42 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	"runtime"
-	"sql2json"
 	"time"
 )
 
 func main() {
-	Reflection()
-	runtime.GC()
-	Mapping()
+	Process()
 }
 
-func Mapping() {
-	fmt.Println("mapping ...")
-	db, mock, _ := sqlmock.New()
-	defer db.Close()
-	rows := sqlmock.NewRows([]string{"Column 1", "Column 2"})
-	for i := 0; i < 10_000_000; i++ {
-		rows.AddRow(fmt.Sprintf("Dummy_%d", i), i)
-	}
-	mock.ExpectQuery("SELECT").WillReturnRows(rows)
-	rs, _ := db.Query("SELECT 1")
-	// start time
-	startTime := time.Now()
-	_, err := sql2json.RowsToJsonMapping(rs)
-	if err != nil {
-		panic(err)
-	}
-	//end time
-	endTime := time.Now()
-	fmt.Printf("Execution Time = %v\n", endTime.Sub(startTime).Milliseconds())
-	printMemUsage()
-}
-
-func Reflection() {
+func Process() {
 	fmt.Println("reflection ...")
 	db, mock, _ := sqlmock.New()
 	defer db.Close()
@@ -48,7 +25,8 @@ func Reflection() {
 	rs, _ := db.Query("SELECT 1")
 	// start time
 	startTime := time.Now()
-	_, err := sql2json.RowsToJsonReflection(rs)
+	// process rows
+	_, err := RowsToJson(rs)
 	if err != nil {
 		panic(err)
 	}
@@ -56,6 +34,28 @@ func Reflection() {
 	endTime := time.Now()
 	fmt.Printf("Execution Time = %v\n", endTime.Sub(startTime).Milliseconds())
 	printMemUsage()
+}
+
+func RowsToJson(rows *sql.Rows) ([]byte, error) {
+	type Dummy struct {
+		Column1 string
+		Column2 string
+	}
+	var result = make([]Dummy, 0)
+	if rows == nil {
+		return nil, fmt.Errorf("rows is nil")
+	}
+	for rows.Next() {
+		var rec = Dummy{}
+		if err := rows.Scan(&rec.Column1, &rec.Column2); err != nil {
+			return nil, err
+		}
+		result = append(result, rec)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return json.Marshal(result)
 }
 
 func printMemUsage() {
