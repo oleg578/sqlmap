c88d9eebefa2fc8d6011eb09c97400a07d8ad1bc
oleg578 <oleg.nagornij@gmail.com>
2024-08-22 14:37:09 +0200

    Refactor main example to limit rows and print JSON output

--- sql2json/example/main.go
+++ sql2json/example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	"runtime"
@@ -12,19 +13,20 @@ func main() {
 	db, mock, _ := sqlmock.New()
 	defer db.Close()
 	rows := sqlmock.NewRows([]string{"Column 1", "Column 2"})
-	for i := 0; i < 10_000_000; i++ {
-		rows.AddRow(fmt.Sprintf("Dummy_%d", i), i)
+	for i := 0; i < 1; i++ {
+		rows.AddRow(fmt.Sprintf("Dummy_%d", i), sql.NullInt64{})
 	}
 	mock.ExpectQuery("SELECT").WillReturnRows(rows)
 	rs, _ := db.Query("SELECT 1")
 	startTime := time.Now()
-	_, err := sql2json.RowsToJson(rs)
+	msg, err := sql2json.RowsToJson(rs)
 	if err != nil {
 		panic(err)
 	}
 	endTime := time.Now()
 	fmt.Printf("Elapsed time: %v ms\n", endTime.Sub(startTime).Milliseconds())
 	printMemUsage()
+	fmt.Println(string(msg))
 
 }
 
