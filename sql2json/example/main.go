d147a00051f9b55139a8fa688e1a9d13054c5765
oleg578 <oleg.nagornij@gmail.com>
2024-08-23 23:16:15 +0200

    Improve data mocking and timing in sql2json example
    Added the faker library to generate realistic data in mock rows. Modified the example to include more detailed columns and calculate elapsed time. Also included a new constant, MOD, for salary and hours calculation, and displayed the length of the JSON output.

--- sql2json/example/main.go
+++ sql2json/example/main.go
@@ -3,30 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-faker/faker/v4"
 	"github.com/oleg578/sqlmap/sql2json"
 	"runtime"
 	"time"
 )
 
 func main() {
+	const MOD = 10000007
 	db, mock, _ := sqlmock.New()
 	defer db.Close()
-	rows := sqlmock.NewRows([]string{"Column 1", "Column 2"})
+	rows := sqlmock.NewRows([]string{"Id", "Name", "Salary", "Hours", "Date"})
 	for i := 0; i < 10_000_000; i++ {
-		rows.AddRow(fmt.Sprintf("Dummy_%d", i), i)
+		rows.AddRow(i, faker.FirstName(), 1000.00*i+(i*678)%MOD, i*8%MOD, faker.Timestamp())
 	}
+	//add null data
+	//rows.AddRow(10, nil, nil, nil, nil)
 	mock.ExpectQuery("SELECT").WillReturnRows(rows)
 	rs, _ := db.Query("SELECT 1")
-	// start time
 	startTime := time.Now()
-	_, err := sql2json.RowsToJson(rs)
+	msg, err := sql2json.RowsToJson(rs)
 	if err != nil {
 		panic(err)
 	}
-	//end time
 	endTime := time.Now()
-	fmt.Printf("Execution Time = %v\n", endTime.Sub(startTime).Milliseconds())
+	fmt.Printf("Elapsed time: %v ms\n", endTime.Sub(startTime).Milliseconds())
 	printMemUsage()
+	fmt.Println(len(msg))
+
 }
 
 func printMemUsage() {
